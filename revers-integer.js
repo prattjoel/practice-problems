906772801eb2c616d885e1db7afa4d29a57416c1
Joel Pratt <pratt.joel@gmail.com>
2019-08-28 21:24:05 -0400

    reverse-integer with backwards for loop.

--- revers-integer.js
+++ revers-integer.js
@@ -21,18 +21,33 @@
 // 
 
 
+// const reverseInteger = (integer) => {
+//   const intString = integer.toString();
+// //  console.log({intString});
+//   const outputStr = intString.split('').reverse().join('');
+// //  console.log({outputStr});
+//   if (outputStr[outputStr.length-1] === '-'){
+//     output = parseInt('-' + outputStr.slice(0, outputStr.length-1))
+//     // console.log({output});
+//     return (output >= Math.pow(-2, 31)) ? output : 0;
+//   }
+//   return parseInt(outputStr) <= (Math.pow(2, 31) - 1) ? outputStr : 0;
+// }
+
 const reverseInteger = (integer) => {
-  const intString = integer.toString();
-//  console.log({intString});
-  const outputStr = intString.split('').reverse().join('');
-//  console.log({outputStr});
-  if (outputStr[outputStr.length-1] === '-'){
-    output = parseInt('-' + outputStr.slice(0, outputStr.length-1))
-    // console.log({output});
-    return (output >= Math.pow(-2, 31)) ? output : 0;
+  const intStr = integer.toString();
+  let outputStr = '';
+  const neg = intStr[0] === '-' ? '-' : null;
+  
+  for (let i=(intStr.length-1); i >= 0; i--){
+    outputStr += intStr[i];
   }
-  return parseInt(outputStr) <= (Math.pow(2, 31) - 1) ? outputStr : 0;
+  
+  if (neg) outputStr = '-' + outputStr.slice(0, outputStr.length-1);
+  
+  return parseInt(outputStr) <= (Math.pow(2, 31) - 1) && parseInt(outputStr) >= Math.pow(-2, 31) ? parseInt(outputStr) : 0;
 }
+
 
 console.log(reverseInteger(123));
 console.log(reverseInteger(-123));
